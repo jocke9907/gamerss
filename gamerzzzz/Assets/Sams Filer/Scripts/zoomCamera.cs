f5337daf032a40144aedaa45ce1996ff389f764a
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-03-06 13:32:36 +0100

    cameraMan

--- gamerzzzz/Assets/Sams Filer/Scripts/zoomCamera.cs
+++ gamerzzzz/Assets/Sams Filer/Scripts/zoomCamera.cs
@@ -23,27 +23,51 @@ public class zoomCamera : MonoBehaviour
     //--------------------------------------------------------------
 
 
-    public Transform target1;
-    public Transform target2;
-    
+    public Transform player1;
+    public Transform player2;
+    public Transform player3;
+    public Transform player4;
+
+    private Transform minDisPlayer;
+    private Transform maxDisPlayer;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
     public float distance;
     Camera camera;
+    public float distanceTest;
 
-    //private void LateUpdate()
-    //{
+    public float minFOV = 30f;
+    public float maxFOV = 60f;
 
-    //    transform.position = target1.position + offset;
-    //}
+    float p1Dis;
+    float p2Dis;
+    float p3Dis;
+    float p4Dis;
+
+    private void Start()
+    {
+
+
+    }
 
 
     private void LateUpdate()
     {
-        distance = Mathf.Sqrt((target1.position.x - target2.position.x) * (target1.position.x - target2.position.x) +
-            (target1.position.z + 10 - target2.position.z) * (target1.position.z + 10 - target2.position.z));
+        //räkna ut vem av targets som är minPlayer och maxPlayer
+        //-----------------------------------------
+        MaxDisChecker();
+        MinDisChecker();
 
-        if(distance * 1.25f < 35)
+
+
+        //-----------------------------FOW beroende på hur långt ifrån spelarna är från varandra.
+        distance = Mathf.Sqrt((maxDisPlayer.position.x - minDisPlayer.position.x) * (maxDisPlayer.position.x - minDisPlayer.position.x) +
+            (maxDisPlayer.position.z + 10 - minDisPlayer.position.z) * (maxDisPlayer.position.z + 10 - minDisPlayer.position.z));
+
+        //distance = Mathf.Sqrt((player1.position.x - player2.position.x) * (player1.position.x - player2.position.x) +
+        //    (player1.position.z + 10 - player2.position.z) * (player1.position.z + 10 - player2.position.z));
+
+        if (distance * 1.25f < 35)
         {
             GetComponent<Camera>().fieldOfView = 35;
         }
@@ -56,10 +80,73 @@ public class zoomCamera : MonoBehaviour
             GetComponent<Camera>().fieldOfView = distance * 1.25f;
         }
 
-        //GetComponent<Camera>().fieldOfView= distance*1.25f;
-        offset = new Vector3((target1.position.x + target2.position.x) / 2, 40, (target1.position.z + target2.position.z) / 2);
+ //------------kameran centrerar i mitten alla spelare 
+        offset = new Vector3((player1.position.x + player2.position.x + player3.position.x + player4.position.x) / 4,
+            40,
+            (player1.position.z + player2.position.z + player3.position.z + player4.position.z) / 4);
 
         transform.position = offset;
+
+
+        distanceTest = Mathf.Sqrt(Mathf.Pow(offset.x, 2) + Mathf.Pow(offset.y, 2) + Mathf.Pow(offset.z, 2));
+
+    }
+
+
+
+
+
+
+    private void MaxDisChecker()
+    {
+        p1Dis = player1.position.x + player1.position.z;
+        p2Dis = player2.position.x + player2.position.z;
+        p3Dis = player3.position.x + player3.position.z;
+        p4Dis = player4.position.x + player4.position.z;
+
+//-----------MAX------------------------------------
+        if (p1Dis > p2Dis && p1Dis > p3Dis && p1Dis > p4Dis)
+        {
+            maxDisPlayer = player1;
+        }
+        else if (p2Dis > p1Dis && p2Dis > p3Dis && p1Dis > p4Dis)
+        {
+            maxDisPlayer = player2;
+        }
+        else if (p3Dis > p1Dis && p3Dis > p2Dis && p1Dis > p4Dis)
+        {
+            maxDisPlayer = player3;
+        }
+        else if (p4Dis > p1Dis && p4Dis > p2Dis && p4Dis > p3Dis)
+        {
+            maxDisPlayer = player4;
+        }
+
+    }
+//----------MIN-----------------------------
+    private void MinDisChecker()
+    {
+        p1Dis = player1.position.x + player1.position.z;
+        p2Dis = player2.position.x + player2.position.z;
+        p3Dis = player3.position.x + player3.position.z;
+        p4Dis = player4.position.x + player4.position.z;
+
+        if (p1Dis < p2Dis && p1Dis < p3Dis && p1Dis < p4Dis)
+        {
+            minDisPlayer = player1;
+        }
+        else if (p2Dis < p1Dis && p2Dis < p3Dis && p2Dis < p4Dis)
+        {
+            minDisPlayer = player2;
+        }
+        else if (p3Dis < p1Dis && p3Dis < p2Dis && p3Dis < p4Dis)
+        {
+            minDisPlayer = player3;
+        }
+        else if (p4Dis < p1Dis && p4Dis < p2Dis && p4Dis < p3Dis)
+        {
+            minDisPlayer = player4;
+        }
     }
 
 }
