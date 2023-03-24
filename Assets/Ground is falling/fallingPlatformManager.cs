69d9348783b74218a9aa60d8b2c44053a29b5671
ViggeGee <victorgustafsson65@gmail.com>
2023-03-24 12:01:20 +0100

    överföring

---
+++ Assets/Ground is falling/fallingPlatformManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatformManager : MonoBehaviour
+{
+
+    List<GameObject> platforms = new List<GameObject>();
+    List<Rigidbody> rbs = new List<Rigidbody>();
+    List<ParticleSystem> particles = new List<ParticleSystem>();
+    float particleTimer = 3.0f;
+    float fallingTimer = 5.0f;
+    int randomObj;
+    float speedup = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.tag == "Ground")
+            {
+                platforms.Add(child.gameObject);
+            }
+        }
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.tag == "Particle")
+            {
+
+            }
+        }
+
+
+        for (int i = 0; i < platforms.Count; i++)
+        {
+            particles.Add(new ParticleSystem());
+            particles[i] = platforms[i].GetComponentInChildren<ParticleSystem>();
+            rbs.Add(new Rigidbody());
+            rbs[i] = platforms[i].GetComponent<Rigidbody>();
+        }
+        randomObj = Random.Range(0, platforms.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fallingTimer -= Time.deltaTime;
+        particleTimer -= Time.deltaTime;
+
+        //if (particleTimer <= 0.0f)
+        //{
+        //    particleTimer = 3.0f - speedup;
+        //    Debug.Log("BING");
+        //    particles[randomObj].Play();
+        //}
+
+        if (fallingTimer <= 0.0f)
+        {
+            fallingTimer = 5.0f - speedup;
+            rbs[randomObj].useGravity = true;
+            randomObj = Random.Range(0, platforms.Count);
+            particles[randomObj].Play();
+
+            if (speedup <= 3.0f)
+            {
+                speedup = speedup + 0.5f;
+            }
+        }
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -103,7 +103,7 @@ public class PlayerController : MonoBehaviour
 
         //HandleInteractions();
         //bomberInput.UpdateTo();
-        bomberInput.UpdateTo();
+        //bomberInput.UpdateTo();
 
     }
 
