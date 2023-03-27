9e907bb7bdecc1f5d60d55796141e96c476904e6
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-03-27 21:52:53 +0200

    har fixat så att kameran är vinklad. Har implementerat en grab funktion (behöver förbättras).

---
+++ Assets/Sams Filer/Scripts/objectGrabber.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectGrabber : MonoBehaviour
+{
+
+    //public KeyCode grabButton;
+    //public float grabDistance = 1.5f;
+
+    //private bool isGrabbing = false;
+    //private GameObject grabbedObject = null;
+
+
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(grabButton))
+    //    {
+    //        if (isGrabbing)
+    //        {
+    //            ReleaseObject();
+    //        }
+    //        else
+    //        {
+    //            MoveObject();
+    //        }
+    //    }
+    //}
+
+    //void GrabObject()
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(transform.position,transform.forward, out hit, grabDistance))
+    //    {
+    //        if (hit.collider.gameObject.GetComponent<Rigidbody>() != null)
+    //        {
+    //            grabbedObject = hit.collider.gameObject;
+    //            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+    //            isGrabbing = true;
+    //        }
+    //    }
+    //}
+
+    //void MoveObject()
+    //{
+    //    grabbedObject.transform.position = transform.position + transform.forward * grabDistance;
+    //}
+
+    //void ReleaseObject()
+    //{
+    //    grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+    //    grabbedObject = null;
+    //    isGrabbing = false;
+    //}
+    public float maxGrabDistance = 0.5f;
+    public KeyCode grabButton = KeyCode.E;
+
+    private GameObject grabbedObject = null;
+    private Vector3 objectOffset = Vector3.zero;
+
+    public movementPilar movementScript;
+    
+    
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(grabButton))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, maxGrabDistance))
+            {
+                
+                grabbedObject = hit.collider.gameObject;
+                
+                objectOffset = grabbedObject.transform.position - transform.position;
+
+            }
+        }
+
+        if (Input.GetKeyUp(grabButton) && grabbedObject != null)
+        {
+            grabbedObject = null;
+        }
+
+        if (grabbedObject != null && grabbedObject.tag=="Movable object")
+        {
+            movementScript.speed = (int)2.5;
+            //grabbedObject.transform.position = transform.position + objectOffset;
+            grabbedObject.transform.position = transform.position + new Vector3(0, 1f, 2);
+        }
+        else
+        {
+            movementScript.speed = (int)5;
+        }
+    }
+}
