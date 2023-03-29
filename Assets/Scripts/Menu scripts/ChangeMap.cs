5fd1c1469d30a6378c61b6d84df49c599288dad2
Arvid98 <111972825+Arvid98@users.noreply.github.com>
2023-03-29 12:28:24 +0200

    Add explotion and lobby change map

---
+++ Assets/Scripts/Menu scripts/ChangeMap.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMap : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask playerLayer;
+
+    [SerializeField] bool bomber;
+    [SerializeField] bool groundIsFalling;
+
+    PlayerController playerController;
+
+    public void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+    public void Update()
+    {
+        if (bomber)
+        {
+            ChangeMapToBomber();
+        }
+       
+
+        if (groundIsFalling)
+        {
+            ChangeMapToGroundFalling();
+        }
+        
+
+    }
+    public void ChangeMapToBomber()
+    {
+
+        float maxDistans = 12f;
+        Vector3 dir = new Vector3(0, 1f, 0);
+
+        if (Physics.Raycast(transform.position, dir, out RaycastHit raycastHitPlayer, maxDistans, playerLayer))
+        {
+            
+            if (raycastHitPlayer.transform.TryGetComponent(out PlayerController playerController))
+            {
+                Loader.Load(Loader.Scene.BomberGame);
+                Loader.bomberGamePlaying = true;
+
+            }
+
+        }
+    }
+    public void ChangeMapToGroundFalling()
+    {
+
+        float maxDistans = 12f;
+        Vector3 dir = new Vector3(0, 1f, 0);
+
+        if (Physics.Raycast(transform.position, dir, out RaycastHit raycastHitPlayer, maxDistans, playerLayer))
+        {
+
+            if (raycastHitPlayer.transform.TryGetComponent(out PlayerController playerController))
+            {
+                Loader.Load(Loader.Scene.ViggesScene);
+                
+
+            }
+
+        }
+    }
+
+}
