55eab465d1e797eb408b2797ca5eb0c5f5d2447f
Tilda <112410519+tildasvardh@users.noreply.github.com>
2023-04-07 18:49:50 +0200

    moving camera

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    public float rotationSpeed = 1.0f;
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        startRotation = transform.rotation;
+        targetRotation = startRotation * Quaternion.Euler(0, 0, 90);
+        StartCoroutine(RotateEveryFiveSeconds());
+    }
+
+    IEnumerator RotateEveryFiveSeconds()
+    {
+        while (true)
+        {
+            float elapsedTime = 0.0f;
+            while (elapsedTime < 5.0f)
+            {
+                transform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime / 5.0f);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.rotation = targetRotation;
+            startRotation = targetRotation;
+            targetRotation *= Quaternion.Euler(0, 0, 90);
+        }
+    }
+}
