3a06066997e628e8e33b3943ac574b1155b70bb1
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-04-12 10:56:51 +0200

    player grabbing

--- Assets/Scripts/Player scripts/PlayerController.cs
+++ Assets/Scripts/Player scripts/PlayerController.cs
@@ -48,6 +48,22 @@ public class PlayerController : MonoBehaviour
     public bool inputE = false;
 
 
+    //------------Sam----------------------------------
+    public float maxGrabDistance = 1f;
+    public KeyCode grabButton = KeyCode.Tab;
+
+    private GameObject grabbedObject = null;
+    private Vector3 objectOffset = Vector3.zero;
+
+    //public movementPilar movementScript;
+    public Transform grabPoint;
+    public LayerMask movable;
+    BoxCollider bc;
+    Rigidbody rb;
+    float test;
+//--------------------------------------------------
+
+
     private void Start()
     {
         bomberInput = FindObjectOfType<BomberInput>();
@@ -117,15 +133,58 @@ public class PlayerController : MonoBehaviour
         }
         if(Loader.wallClimberPlaying == true)
         {
-            wallClimberInput.UpdateWallClimberTo();
+            //wallClimberInput.UpdateWallClimberTo();
         }
-       
+
+        GrabObject();
+    }
+    private void GrabObject()
+    {
+
+        if (Input.GetKeyDown(grabButton))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, maxGrabDistance, movable))
+            {
+
+                grabbedObject = hit.collider.gameObject;
+
+                bc = grabbedObject.GetComponent<BoxCollider>();
+                rb = grabbedObject.GetComponent<Rigidbody>();
+
+            }
+        }
+
+        if (Input.GetKeyUp(grabButton) && grabbedObject != null)
+        {
+            grabbedObject = null;
+        }
+
+        if (grabbedObject != null)
+        {
+            bc.isTrigger = true;
+            //grabbedObject.transform.rotation = Quaternion.identity.x;
+            //grabbedObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            Quaternion newRotation = Quaternion.Euler(0f, grabPoint.rotation.eulerAngles.y, 0f);
+            grabbedObject.transform.rotation = newRotation;
+            //rb.useGravity = false;
+            //movementScript.speed = (int)2.5;
+            grabbedObject.transform.position = grabPoint.position;
+
+        }
+        else
+        {
+            bc.isTrigger = false;
+            //rb.useGravity = true;
+            //movementScript.speed = (int)5;
+        }
+
 
     }
 
 
-   
-    
+
+
 
     ///!!!!!!! ta inte bort
 
