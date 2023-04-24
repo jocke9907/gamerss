91bd768cec6885b16851b72815295f40dab28c72
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-04-25 00:01:54 +0200

    Update RandomMap.cs

--- Assets/Scripts/Menu scripts/RandomMap.cs
+++ Assets/Scripts/Menu scripts/RandomMap.cs
@@ -48,7 +48,7 @@ public class RandomMap : MonoBehaviour
         //bomberManger.captureTheFlagPlayed = true;
 
         // ändra denna för att byta map
-        randMap = 1;
+        randMap = 4;
         //
         if (!bomberManger.fallinggroundPlayed && randMap == 1)
         {
