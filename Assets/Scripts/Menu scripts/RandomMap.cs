1813a50c14ba4200d47688a1921abce048d9f788
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-05-03 12:29:01 +0200

    randomMap fix

--- Assets/Scripts/Menu scripts/RandomMap.cs
+++ Assets/Scripts/Menu scripts/RandomMap.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class RandomMap : MonoBehaviour
 {
     BomberManger bomberManger;
-    float targetTime3 = 5f;
-    //bool giveScore = true;
+    float targetTime3 = 11f;
+    bool giveScore = true;
+    bool changeMap = false;
+    bool mapSelected;
+    public int randMap;
     private void Awake()
     {
         bomberManger = FindObjectOfType<BomberManger>();
@@ -17,91 +20,150 @@ public class RandomMap : MonoBehaviour
             player.veryDead = false;
             //player.gameObject.SetActive(true);
         }
-        //giveScore = true;
-        
+        giveScore = true;
+
 
     }
     private void Start()
     {
-        
+
     }
     void Update()
     {
 
-        
-
-
         targetTime3 -= Time.deltaTime;
 
-        if (targetTime3 < 0)
+        if (targetTime3 < 10)
         {
             ChangeMaps();
+        }
+        if (targetTime3 <= 0)
+        {
+            changeMap = true;
         }
     }
     void ChangeMaps()
     {
-       
-        
-        Loader.TheMazePlaying = false;
 
-        int randMap = Random.Range(1, 5);
+
+        Loader.TheMazePlaying = false;
+        if (mapSelected == false)
+        {
+            randMap = Random.Range(1, 6);
+        }
+
         //bomberManger.captureTheFlagPlayed = true;
 
         // ändra denna för att byta map
-        randMap = 4;
+        //randMap = 5;
         //
         if (!bomberManger.fallinggroundPlayed && randMap == 1)
         {
-            bomberManger.fallinggroundPlayed = true;
-            Loader.PlatformGamePlaying = true;
-            Loader.Load(Loader.Scene.ViggesScene);
-
+            mapSelected = true;
+            if (changeMap)
+            {
+                bomberManger.redusePlayers = false;
+                bomberManger.fallinggroundPlayed = true;
+                Loader.PlatformGamePlaying = true;
+                Loader.Load(Loader.Scene.ViggesScene);
+            }
         }
         else if (!bomberManger.bomberPlayed && randMap == 2)
         {
-            bomberManger.bomberPlayed = true;
-            bomberManger.GameStart();
-            if (CreateGameUI.onePlayer == true)
+            mapSelected = true;
+            if (changeMap)
             {
-                bomberManger.playerCountBomber = 1;
+                bomberManger.redusePlayers = false;
+                bomberManger.bomberPlayed = true;
+                bomberManger.GameStart();
+                bomberManger.bomberPoints = 4;
+                if (CreateGameUI.onePlayer == true)
+                {
+                    bomberManger.playerCountBomber = 1;
+                }
+                if (CreateGameUI.twoPlayer == true)
+                {
+                    bomberManger.playerCountBomber = 1;
+                }
+                if (CreateGameUI.threePlayer == true)
+                {
+                    bomberManger.playerCountBomber = 2;
+                }
+                if (CreateGameUI.fourPlayer == true)
+                {
+                    bomberManger.playerCountBomber = 3;
+                }
+                Loader.bomberGamePlaying = true;
+                Loader.Load(Loader.Scene.BomberGame);
             }
-            if (CreateGameUI.twoPlayer == true)
-            {
-                bomberManger.playerCountBomber = 1;
-            }
-            if (CreateGameUI.threePlayer == true)
-            {
-                bomberManger.playerCountBomber = 2;
-            }
-            if (CreateGameUI.fourPlayer == true)
-            {
-                bomberManger.playerCountBomber = 3;
-            }
-            Loader.bomberGamePlaying = true;
-            Loader.Load(Loader.Scene.BomberGame);
         }
-        else if (!bomberManger.captureTheFlagPlayed && randMap == 5)
+        else if (!bomberManger.captureTheFlagPlayed && randMap == 6)
         {
-            bomberManger.captureTheFlagPlayed = true;
-            Loader.Load(Loader.Scene.CaptureTheFlag);
+            mapSelected = true;
+            if (changeMap)
+            {
+                bomberManger.captureTheFlagPlayed = true;
+                Loader.Load(Loader.Scene.CaptureTheFlag);
+            }
         }
         else if (!bomberManger.mazePlayed && randMap == 3)
         {
-            bomberManger.mazePlayed = true;
-            Loader.TheMazePlaying = true;
-            Loader.wallClimberPlaying = false;
-            Loader.Load(Loader.Scene.TheMaze);
+            mapSelected = true;
+            if (changeMap)
+            {
+                bomberManger.mazePlayed = true;
+                Loader.TheMazePlaying = true;
+                Loader.wallClimberPlaying = false;
+                Loader.Load(Loader.Scene.TheMaze);
+            }
         }
         else if (!bomberManger.wallClimerPlayed && randMap == 4)
         {
-            bomberManger.wallClimerPlayed = true;
-            Loader.wallClimberPlaying = true;
-            //Loader.TheMazePlaying = false;
-            Loader.Load(Loader.Scene.SamScen);
+            mapSelected = true;
+            if (changeMap)
+            {
+                bomberManger.wallClimerPlayed = true;
+                Loader.wallClimberPlaying = true;
+                //Loader.TheMazePlaying = false;
+                Loader.Load(Loader.Scene.SamScen);
+            }
+
+        }
+        else if (!bomberManger.lavaGroundPlayed && randMap == 5)
+        {
+            mapSelected = true;
+            if (changeMap)
+            {
+                bomberManger.redusePlayers = false;
+                bomberManger.lavaGroundPlayed = true;
+                bomberManger.GameStart();
+                bomberManger.bomberPoints = 4;
+                if (CreateGameUI.onePlayer == true)
+                {
+                    bomberManger.playerCountBomber = 1;
+                }
+                if (CreateGameUI.twoPlayer == true)
+                {
+                    bomberManger.playerCountBomber = 1;
+                }
+                if (CreateGameUI.threePlayer == true)
+                {
+                    bomberManger.playerCountBomber = 2;
+                }
+                if (CreateGameUI.fourPlayer == true)
+                {
+                    bomberManger.playerCountBomber = 3;
+                }
+                Loader.LavaGroundPlaying = true;
+                Loader.Load(Loader.Scene.LavaGround);
+            }
         }
         else
         {
-            randMap = Random.Range(1, 5);
+            if (mapSelected == false)
+            {
+                randMap = Random.Range(1, 6);
+            }
         }
     }
 }
