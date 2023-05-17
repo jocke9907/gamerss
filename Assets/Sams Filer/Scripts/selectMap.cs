ce88a10f642bf322225ce148da6110a9d0084dfb
SamShariati <114943247+SamShariati@users.noreply.github.com>
2023-05-18 00:10:02 +0200

    Fixat beskrivning UI till varje bana, aktiveras när man står på en pelare.

--- Assets/Sams Filer/Scripts/selectMap.cs
+++ Assets/Sams Filer/Scripts/selectMap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class selectMap : MonoBehaviour
@@ -13,13 +14,27 @@ public class selectMap : MonoBehaviour
     public bool CaptureFlag;
     public bool WheelSpinner;
     public bool LavaRun;
+
+    [SerializeField] TMP_Text groundFallingText;
+    [SerializeField] TMP_Text mazeRunnerText;
+    [SerializeField] TMP_Text wallClimberText;
+    [SerializeField] TMP_Text bomberManText;
+    [SerializeField] TMP_Text captureFlagText;
+    [SerializeField] TMP_Text wheelSpinner;
+    [SerializeField] TMP_Text lavaRunText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        groundFallingText.enabled = false;
+        mazeRunnerText.enabled = false;
+        wallClimberText.enabled = false;
+        bomberManText.enabled = false;
+        captureFlagText.enabled = false;
+        wheelSpinner.enabled = false;
+        lavaRunText.enabled = false;
     }
 
-    private void OnTriggerEnter(Collider player)
+    private void OnTriggerStay(Collider player)
     {
         if (RandomMap)
         {
@@ -30,42 +45,78 @@ public class selectMap : MonoBehaviour
         {
             player.GetComponent<PlayerController>().mapSelected = "Falling Ground";
             player.GetComponent<PlayerController>().mapSelectedInt = 1;
+            groundFallingText.enabled = true;
         }
         else if (MazeRunner)
         {
             player.GetComponent<PlayerController>().mapSelected = "Maze Runner";
             player.GetComponent<PlayerController>().mapSelectedInt = 2;
+            mazeRunnerText.enabled = true;
         }
         else if (WallClimber)
         {
             player.GetComponent<PlayerController>().mapSelected = "Wall Climber";
             player.GetComponent<PlayerController>().mapSelectedInt = 3;
+            wallClimberText.enabled = true;
         }
         else if (BomberMan)
         {
             player.GetComponent<PlayerController>().mapSelected = "Bomber Man";
             player.GetComponent<PlayerController>().mapSelectedInt = 4;
+            bomberManText.enabled = true;
         }
         else if (CaptureFlag)
         {
             player.GetComponent<PlayerController>().mapSelected = "Capture Flag";
             player.GetComponent<PlayerController>().mapSelectedInt = 5;
+            captureFlagText.enabled = true;
         }
         else if (WheelSpinner)
         {
             player.GetComponent<PlayerController>().mapSelected = "Wheel Spinner";
             player.GetComponent<PlayerController>().mapSelectedInt = 6;
+            wheelSpinner.enabled = true;
         }
         else if (LavaRun)
         {
             player.GetComponent<PlayerController>().mapSelected = "Lava Run";
             player.GetComponent<PlayerController>().mapSelectedInt = 7;
+            lavaRunText.enabled = true;
         }
 
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider player)
     {
-        other.GetComponent<PlayerController>().mapSelected = "";
-        other.GetComponent<PlayerController>().mapSelectedInt =-1;
+        player.GetComponent<PlayerController>().mapSelected = "";
+        player.GetComponent<PlayerController>().mapSelectedInt =-1;
+
+        if (GroundIsFalling)
+        {
+            groundFallingText.enabled = false;
+        }
+        else if (MazeRunner)
+        {
+            mazeRunnerText.enabled = false;
+        }
+        else if (WallClimber)
+        {
+            wallClimberText.enabled = false;
+        }
+        else if (BomberMan)
+        {
+            bomberManText.enabled = false;
+        }
+        else if (CaptureFlag)
+        {
+            captureFlagText.enabled = false;
+        }
+        else if (WheelSpinner)
+        {
+            wheelSpinner.enabled = false;
+        }
+        else if (LavaRun)
+        {
+            lavaRunText.enabled = false;
+        }
     }
 }
